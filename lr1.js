2adeff852ad8c62c7f62ab368b6f9bb8c52eddbb
Eugene Kurlovich <zhenikpggkurlovich@gmail.com>
2017-09-26 10:01:06 +0300

    print all line arguments

--- lr1.js
+++ lr1.js
@@ -2,3 +2,9 @@ console.log('Hello World');
 
 const name = process.argv[2];
 console.log(`Hi ${name}!`);
+
+
+for (let i = 0; i < process.argv.length- 2; i ++)
+{
+    console.log("Buffer: " + process.argv[i+2]);
+}

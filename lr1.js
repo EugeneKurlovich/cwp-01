7ebe80dbe877be8775360e5b46c7c542707838fe
Eugene Kurlovich <zhenikpggkurlovich@gmail.com>
2017-09-26 10:04:18 +0300

    create ScriptFile

--- lr1.js
+++ lr1.js
@@ -8,3 +8,44 @@ for (let i = 0; i < process.argv.length- 2; i ++)
 {
     console.log("Buffer: " + process.argv[i+2]);
 }
+
+
+var fs = require('fs');
+var path = require('path');
+var pd = process.argv[2];
+
+
+
+
+if (pd != undefined)
+{
+    fs.stat(pathDirectory, 
+    function (error,stats)
+        {
+            if (error || !(stats.isDirectory()))
+            {
+                console.log("Неверный путь!!!!");
+            }
+            else
+            {
+                fs.writeFile(pd + "\\eugene.js", myScript, 
+                function(error)
+                {
+                    if (error)
+                    {
+                        console.log("Ошибка создания файла");
+                    }
+                }
+                )
+
+                console.log("node " + pd + "\\eugene.js");
+            }
+        }    
+    )
+
+}
+
+else
+{
+        console.log ("Введите путь к файлу!!!");
+}
